4af70d471f9f504f82a37c7772b25c78519e1069
Ubuntu <molly@mollysUbuntu.wuqc3rxj1eeunhjy3dnfgwk2qh.ex.internal.cloudapp.net>
2016-02-25 20:10:02 +0000

    change port number

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express'); 
 var app = express(); 
  
-var port = 3000 || process.env.PORT; 
+var port = 8080 || process.env.PORT; 
 
 app.use(express.static(__dirname + '/public')); 
  
