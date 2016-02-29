ccec8672a249b2fc0a99f46fc5878a30a1e09913
Molly Kent <v-mokent@microsoft.com>
2016-02-29 20:07:40 +0000

    move script file into public

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 var fs = require("fs");
 var data = fs.readFileSync("./data.txt").toString();
+data = data.slice(0, -2);
 console.log(data);
 //var parsed = JSON.parse(data);
 //console.log(parsed);
