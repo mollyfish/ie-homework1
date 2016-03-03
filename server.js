fe946c3393cc428b168ca3d07cde8ee4464f38cc
Molly Kent (InConsulting Inc.) <v-mokent@microsoft.com>
2016-03-03 14:01:54 -0800

    console check in server

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(bodyParser());
 
 
 app.get('/cpuinfo', function(req, res) {
+	console.log('i hear you');
 	exec(path.join(__dirname + '/get-cpu.sh'));
   fs.readFile(path.join(__dirname + '/public/data.json'), function(err, data){
   	console.log("packet");
