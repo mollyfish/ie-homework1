5ad0f101c93e00f68116c82dfa830d5d0c0e5cce
Molly Kent (InConsulting Inc.) <v-mokent@microsoft.com>
2016-03-03 13:45:05 -0800

    reset child process require, remove deleter from server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var fs = require('fs');
 var path = require('path');
 var bodyParser = require('body-parser');
-var child_process = require('child_process'); 
+var exec = require('child_process').exec; 
 var port = 8080 || process.env.PORT; 
 
 app.use(express.static(__dirname + '/public'));
@@ -11,11 +11,10 @@ app.use(bodyParser());
 
 
 app.get('/cpuinfo', function(req, res) {
-	child_process.exec(path.join(__dirname + '/get-cpu.sh'));
+	exec(path.join(__dirname + '/get-cpu.sh'));
   fs.readFile(path.join(__dirname + '/public/data.json'), function(err, newData){
   	console.log("packet");
   	res.send(newData);
-  	child_process.exec(path.join(__dirname + '/delete-json.sh'));
   });
 });
 
