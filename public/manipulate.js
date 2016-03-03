23285c213a59d9d69a0ada51adae4958917ef6cd
Molly Kent (InConsulting Inc.) <v-mokent@microsoft.com>
2016-03-03 13:08:49 -0800

    clean up

--- public/manipulate.js
+++ public/manipulate.js
@@ -13,20 +13,18 @@ var prevCpus = {
 var cpus = {};
 var percentages = [0, 0, 0, 0, 0, 0, 0];
 var data = {
-    labels: ["-60", "-50", "-40", "-30", "-20", "-10", "0"],
-    datasets: [
-        {
-            label: "CPU Usage",
-            fillColor: "rgba(107,13,255,0.2)",
-            strokeColor: "rgba(107,13,255,1)",
-            pointColor: "rgba(107,13,255,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(107,13,255,1)",
-            // data: []
-            data: [percentages[0],percentages[1],percentages[2],percentages[3],percentages[4],percentages[5],percentages[6]]
-        }
-    ]
+  labels: ["-60", "-50", "-40", "-30", "-20", "-10", "0"],
+  datasets: [{
+      label: "CPU Usage",
+      fillColor: "rgba(107,13,255,0.2)",
+      strokeColor: "rgba(107,13,255,1)",
+      pointColor: "rgba(107,13,255,1)",
+      pointStrokeColor: "#fff",
+      pointHighlightFill: "#fff",
+      pointHighlightStroke: "rgba(107,13,255,1)",
+      // data: []
+      data: [percentages[0],percentages[1],percentages[2],percentages[3],percentages[4],percentages[5],percentages[6]]
+    }]
 };
 var options = {
     scaleOverride: true,
@@ -34,77 +32,66 @@ var options = {
     scaleStepWidth: 10,
     scaleStartValue: 0,
 };
+
+
 var ctx = document.getElementById("myChart").getContext("2d");
 var myNewChart = new Chart(ctx).Line(data, options);
 
 
 setInterval(function() {
+  $.get('/cpuinfo', function(newData) {
+    console.log(JSON.parse(newData));
+    
+    cpus = JSON.parse(newData);
+    // console.dir(prevCpus);
+    
+    var prevSoftirq = parseInt(prevCpus.softirq);
+    var prevGuest_nice = parseInt(prevCpus.guest_nice);
+    var prevUser = parseInt(prevCpus.user);
+    var prevGuest = parseInt(prevCpus.guest);
+    var prevSteal = parseInt(prevCpus.steal);
+    var prevSystem = parseInt(prevCpus.system);
+    var prevNice = parseInt(prevCpus.nice);
+    var prevIrq = parseInt(prevCpus.irq);
+    var prevIowait = parseInt(prevCpus.iowait);
+    var prevIdle = parseInt(prevCpus.idle);
 
-$.get('/cpuinfo', function(newData) {
-  cpus = JSON.parse(newData);
-  console.dir(prevCpus);
-  
+    var softirq = parseInt(cpus.softirq);
+    var guest_nice = parseInt(cpus.guest_nice);
+    var user = parseInt(cpus.user);
+    var guest = parseInt(cpus.guest);
+    var steal = parseInt(cpus.steal);
+    var system = parseInt(cpus.system);
+    var nice = parseInt(cpus.nice);
+    var irq = parseInt(cpus.irq);
+    var iowait = parseInt(cpus.iowait);
+    var idle = parseInt(cpus.idle);
 
-var prevSoftirq = parseInt(prevCpus.softirq);
-var prevGuest_nice = parseInt(prevCpus.guest_nice);
-var prevUser = parseInt(prevCpus.user);
-var prevGuest = parseInt(prevCpus.guest);
-var prevSteal = parseInt(prevCpus.steal);
-var prevSystem = parseInt(prevCpus.system);
-var prevNice = parseInt(prevCpus.nice);
-var prevIrq = parseInt(prevCpus.irq);
-var prevIowait = parseInt(prevCpus.iowait);
-var prevIdle = parseInt(prevCpus.idle);
+    var trueIdle = idle + iowait;
+    var prevTrueIdle = prevIdle + prevIowait;
 
-var softirq = parseInt(cpus.softirq);
-var guest_nice = parseInt(cpus.guest_nice);
-var user = parseInt(cpus.user);
-var guest = parseInt(cpus.guest);
-var steal = parseInt(cpus.steal);
-var system = parseInt(cpus.system);
-var nice = parseInt(cpus.nice);
-var irq = parseInt(cpus.irq);
-var iowait = parseInt(cpus.iowait);
-var idle = parseInt(cpus.idle);
+    var nonIdle = user + nice + system + irq + softirq + steal;
+    var prevNonIdle = prevUser + prevNice + prevSystem + prevIrq + prevSoftirq + prevSteal
 
-var trueIdle = idle + iowait;
-var prevTrueIdle = prevIdle + prevIowait;
+    var total = trueIdle + nonIdle;
+    var prevTotal = prevTrueIdle + prevNonIdle;
 
-var nonIdle = user + nice + system + irq + softirq + steal;
-var prevNonIdle = prevUser + prevNice + prevSystem + prevIrq + prevSoftirq + prevSteal
+    var totalDiff = total - prevTotal;
+    var idleDiff = trueIdle - prevTrueIdle;
 
-var total = trueIdle + nonIdle;
-var prevTotal = prevTrueIdle + prevNonIdle;
+    var rawPercentage = (totalDiff - idleDiff) / totalDiff;
+    var prettyPercentage = (rawPercentage*100);
 
-var totalDiff = total - prevTotal;
-var idleDiff = trueIdle - prevTrueIdle;
+    // console.log(prettyPercentage);
 
-var rawPercentage = (totalDiff - idleDiff) / totalDiff;
-var prettyPercentage = (rawPercentage*100);
+    percentages.push(prettyPercentage);
+    percentages.shift();
 
-// console.log(prettyPercentage);
-
-prevCpus = cpus;
-
-console.dir(prevCpus);
-
-percentages.push(prettyPercentage);
-percentages.shift();
-
-// console.log(percentages);
-
-
-
-myNewChart.addData([percentages[6]], "0");
-
-
-data.datasets[0].data = [percentages[0],percentages[1],percentages[2],percentages[3],percentages[4],percentages[5],percentages[6]]; 
-
-
-myNewChart.removeData();
-
-myNewChart.scale.xLabels = ["-60", "-50", "-40", "-30", "-20", "-10", "0"];
-myNewChart.update();
-
-});
+    myNewChart.addData([percentages[6]], "0");
+    data.datasets[0].data = [percentages[0],percentages[1],percentages[2],percentages[3],percentages[4],percentages[5],percentages[6]]; 
+    myNewChart.removeData();
+    myNewChart.scale.xLabels = ["-60", "-50", "-40", "-30", "-20", "-10", "0"];
+    myNewChart.update();
+    prevCpus = cpus;
+  });
 }, 4000);
